3cbbab00a7a8a44e367d60d3e138b9321db6ee8c
leejaewook <leee913@naver.com>
2026-03-20 18:14:27 +0900

    mongo.db

--- 3.mongo.db/models/user_model.py
+++ 3.mongo.db/models/user_model.py
@@ -1,10 +1,10 @@
 from beanie import Document
 from typing import Optional
-
+#컬렉션(Document 상속->MongoDB 컬렉션과 연결)
 class User(Document):
     name : str
     email : str
-    age : Optional[int] = None
+    age : Optional[int] = None #age는 있어도 되고 없어도 된다.
 
     class Settings: # 저장소
         name = "users"
--- 3.mongo.db/routes/user_route.py
+++ 3.mongo.db/routes/user_route.py
@@ -3,19 +3,22 @@ from models.user_model import User
 
 user_route = APIRouter(tags=["User"])
 
+# 모든 데이터 보기
 @user_route.get("/all")
 async def all_user():
     users = User.find_all()
     print("users : ", users)
-    data = await users.to_list()
+    data = await users.to_list() # db에서 조회한 데이터를 리스트로 변환
     print("실제 데이터 : ", data)
     return data
 
+# 데이터 추가
 @user_route.post("/insert")
 async def insert(user : User):
     await user.create()
     return {"id":str(user.id)}
 
+# 특정 데이터 보기(id로)
 @user_route.get("/one/{user_id}")
 async def one_user(user_id : str):
     user = User.get(user_id)
@@ -23,6 +26,7 @@ async def one_user(user_id : str):
         return await user
     return {"msg":"데이터 없음"}
 
+#특정 데이터 보기(name으로(User 컬렉션의 name과 사용자가 입력한 name 비교))
 @user_route.get("/name/{user_name}")
 async def one_user(user_name : str):
     user = await User.find_one(User.name == user_name)
@@ -30,11 +34,13 @@ async def one_user(user_name : str):
         return user
     return {"msg":"데이터 없음"}
 
+# 데이터 삭제
 @user_route.delete("/del/{user_name}")
 async def del_user(user_name : str):
     count = await User.find_one(User.name == user_name).delete()
     return {"msg" : f"삭제 된 수 : {count}"}
 
+# 데이터 수정
 @user_route.put("/update/{user_name}")
 async def update_user(user_name:str, new_age:int):
     user = await User.find_one(User.name == user_name)
